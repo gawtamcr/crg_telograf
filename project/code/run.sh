1880841b634b86738dc6201e9894b39898b309c7
gawtam <gawtamcr3@gmail.com>
2026-03-11 10:22:31 +0100

    cleaning model folder

--- project/code/run.sh
+++ project/code/run.sh
@@ -1,11 +1,11 @@
-python train.py \
+python -B train.py \
         --flow \
         --first_sat_init \
         --skip_first_eval \
         --clip_max 50000 \
         --max_sol_clip 2 \
         --type_ratios 1 3 2 4 \
-        -T g0128-075243_simple_gnn_F \
+        -T simple_gnn_F \
         --num_evals 128 \
         -b 1 \
         --test_muls 1024
